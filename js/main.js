11d9322bf23b3a78800e6a3a4e7b122bf66c56d2
evgeniy <eugendzygew@gmail.com>
2022-01-06 00:53:55 +0300

    fixes

--- js/main.js
+++ js/main.js
@@ -25,15 +25,18 @@ const swiper = new Swiper('.swiper', {
       centeredSlides: true,
 
     },
-/*     350:{
-      centeredSlides: false,
-    }, */
     1150:{
       centeredSlides: false,
     },
     
-  }
+  },
+  observer: true,
+  observeParents: true,
+  observeSlideChildren:true,
+
 });
+
+//add fovorites
 const hearts = document.querySelectorAll('.heart')
 
 hearts.forEach(element => {
@@ -41,3 +44,28 @@ hearts.forEach(element => {
     element.classList.toggle('active')
   })
 });
+
+//tabs
+const tabs = document.querySelectorAll(".tab");
+
+const switchTab = function(event) {
+    event.preventDefault();
+
+    let parentTabs = this.parentNode.children;
+    let parentTabsContent = this.closest('.catalog-filtres').nextElementSibling.children;
+    for (let i = 0; i < parentTabs.length; i++) {
+        parentTabs[i].classList.remove("active");
+    }
+    for (let i = 0; i < parentTabsContent.length; i++) {
+        parentTabsContent[i].classList.remove("active-tab");
+    }
+    this.classList.add("active");
+    let number = this.getAttribute('name');
+    console.log(number);
+    let activeForm = document.querySelector(number);
+    activeForm.classList.add("active-tab");
+};
+
+for (let i = 0; i < tabs.length; i++) {
+    tabs[i].addEventListener("click", switchTab);
+}
